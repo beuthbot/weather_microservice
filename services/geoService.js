18abe17b2b8f128df17ac5661b9032a88f38ed90
Jan Fromme <s76400@beuth-hochschule.de>
2020-07-02 13:23:35 +0200

    geoService fix (used a javascript feature that is not available yet in the current version)

--- services/geoService.js
+++ services/geoService.js
@@ -4,16 +4,24 @@ const config = require('../config')
 module.exports = {
     getCoordinates: async (query) => {
         try {
+            let result = {
+                lat: null,
+                lon: null,
+                display_name: null,
+                query
+            }
+
             let url = `https://nominatim.openstreetmap.org/search?q=${query}&format=json`
             // console.debug(url)
 
-            let place = await axios.get(url)[0]
+            let places = await axios.get(url)
 
-            let result = {
-                lat: place?.lat,
-                lon: place?.lon,
-                display_name: place?.display_name,
-                query
+            if(Array.isArray(places) && places.length > 0) {
+                result = {...result, ...{
+                    lat: places[0].lat,
+                    lon: places[0].lon,
+                    display_name: places[0].display_name
+                }}
             }
 
             return result
